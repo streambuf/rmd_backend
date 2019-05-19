36584be9fbe9715042c0965147a040af403a5d2b
Moiseev Maxim <mamoiseev@8bitgroup.com>
2019-05-20 00:51:58 +0300

    minor

--- src/main/kotlin/ru/rmd/monolith/controller/ExceptionHandlerController.kt
+++ src/main/kotlin/ru/rmd/monolith/controller/ExceptionHandlerController.kt
@@ -1,12 +1,8 @@
 package ru.rmd.monolith.controller
 
-import com.fasterxml.jackson.databind.JsonMappingException
-import com.fasterxml.jackson.databind.exc.InvalidFormatException
-import com.fasterxml.jackson.databind.exc.MismatchedInputException
 import mu.KotlinLogging
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
